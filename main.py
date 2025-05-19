7f8bfa9130610d7af4546f338c4db57e39ba802c
nee-hao <imnotnee@gmail.com>
2025-05-18 20:15:33 -0700

    added player sprite and player movement

--- main.py
+++ main.py
@@ -1,18 +1,30 @@
 import pygame
 from constants import *
+from player import Player
 
 
 def main():
+    # pygame init
     pygame.init()
-    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    game_clock = pygame.time.Clock()
+    dt = 0
+
+    player = Player(x = SCREEN_WIDTH / 2, y = SCREEN_HEIGHT / 2)
+
+    # game loop
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
 
         screen.fill("black")
+        player.draw(screen)
+        player.update(dt)
         pygame.display.flip()
+        dt = game_clock.tick(60) / 1000
+        print(dt)
 
 
 if __name__ == "__main__":
